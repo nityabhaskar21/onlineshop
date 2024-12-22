96b947aaf5e4f267dca6254b86d8227cff791ae2
Nityanand Bhaskar <nbhaskar@altimetrik.com>
2024-12-22 18:33:34 +0530

    Update application configuration file

---
+++ src/main/java/com/example/onlineshop/configs/ApplicationConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.onlineshop.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public RestTemplate createRestTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory ();
+//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+////        requestFactory.setRe(600000);
+//        requestFactory.setConnectTimeout(600000);
+        return new RestTemplate(factory);
+    }
+}
+
